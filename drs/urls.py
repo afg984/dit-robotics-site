32ee47d6c30a4d330dc76bc597436f5f22ee7d9f
afg984 <afg984@gmail.com>
2014-11-23 18:50:47 +0800

    add staticfiles_urlpatterns if settings.DEBUG

--- drs/urls.py
+++ drs/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from . import settings
+
 urlpatterns = patterns('',
     url(r'^$', 'drs.views.home', name='home'),
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^drive/', include('drive.urls')),
 )
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
