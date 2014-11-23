7b06af87b900dbca3181ad87105f075d64c4ecde
afg984 <afg984@gmail.com>
2014-11-23 18:35:19 +0800

    fix NameError in drive/views.py

--- drive/views.py
+++ drive/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def drive(request):
-    data = {title: 'Drive'}
+    data = {'title': 'Drive'}
     return render(request, 'drive.html', data)
