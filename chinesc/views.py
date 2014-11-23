1a33a2384702dd5d2f750a34ec3a3b4abd88a28c
afg984 <afg984@gmail.com>
2014-11-24 01:09:42 +0800

    filling chinesc/views.py

--- chinesc/views.py
+++ chinesc/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from pygments import highlight
+from pygments.lexers import CppLexer
+from pygments.formatters import HtmlFormatter
+
 # Create your views here.
+
+def chinesc(request):
+    form = CodeForm(request.GET)
+    if form.is_valid():
+        formatter = HtmlFormatter(style='default')
+        context = dict(
+            code=highlight(form.user_code, CppLexer(), formatter),
+            style=formatter.get_style_defs('.highlight')
+        )
+    else:
+        return form     
