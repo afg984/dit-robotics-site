e88fda56b1463abf9438f96ee390ef98134db293
afg984 <afg984@gmail.com>
2014-11-23 16:04:31 +0800

    move homepage/ into drs/

--- drs/settings.py
+++ drs/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage',
+    'drs',
 )
 
 MIDDLEWARE_CLASSES = (
--- drs/urls.py
+++ drs/urls.py
@@ -3,9 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'drs.views.home', name='home'),
+    url(r'^$', 'drs.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'homepage.views.home', name='home'),
 )
