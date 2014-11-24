3e49efbb2cdfaa1092e7c949f8cbdafcd7c261d2
afg984 <afg984@gmail.com>
2014-11-24 14:53:32 +0800

    textarea has rows, cols specfication

--- chinesc/forms.py
+++ chinesc/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class CodeForm(forms.Form):
-    user_code = forms.CharField(widget=forms.Textarea)
+    user_code = forms.CharField(
+        widget=forms.Textarea(attrs={'rows':24, 'cols':80})
+    )
