31b353143380b2a25d2b289c36d35087546f3498
afg984 <afg984@gmail.com>
2014-11-24 21:55:52 +0800

    label of user_code set to "Your Code"

--- chinesc/forms.py
+++ chinesc/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 class CodeForm(forms.Form):
     user_code = forms.CharField(
+        label='Your Code',
         widget=forms.Textarea(attrs={'rows':24, 'cols':80})
     )
