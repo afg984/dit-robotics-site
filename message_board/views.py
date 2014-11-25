fa284fee31188d38375146fa780aec4161d24179
afg984 <afg984@gmail.com>
2014-11-25 16:57:55 +0800

    message_board code for testing

--- message_board/views.py
+++ message_board/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Message
+from .forms import MessageForm
 
 # Create your views here.
+def message_board(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('message_board')
+        else:
+            context = dict()
+            context['form'] = form
+    else:
+        context = dict()
+        context['form'] = MessageForm()
+    return render(request, 'message_board.html', context)
