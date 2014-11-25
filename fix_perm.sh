a00cc69e055a9847983150f10e3e39e0a181116d
afg <afg984@gmail.com>
2014-11-25 18:39:06 +0800

    update permissions

--- fix_perm.sh
+++ fix_perm.sh
@@ -1,6 +1,7 @@
-chown afg:http . -R
 chgrp http . -R
 chgrp afg .git -R
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
-chmod 755 manage.py 
+chmod 755 manage.py
+chmod 775 .
+chmod 664 db.sqlite3
