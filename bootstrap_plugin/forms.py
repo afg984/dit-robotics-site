a51aff41126d238edfd084bf15126eaea715b9b6
afg984 <afg984@gmail.com>
2014-11-29 02:49:17 +0800

    make normal_row actually working

--- bootstrap_plugin/forms.py
+++ bootstrap_plugin/forms.py
@@ -1,23 +1,38 @@
 from django.forms.forms import BoundField
 
-def bsCssClasses(self):
-    return super(BoundField, self).css_classes('form-group')
+class BSBoundField(BoundField):
+    def css_classes(self, extra_classes=None):
+        if hasattr(extra_classes, 'split'):
+            extra_classes = extra_classes.split()
+        extra_classes = set(extra_classes or [])
+        extra_classes.add('form-group')
+        return super().css_classes(extra_classes)
+
 
 def simpleFactory(cls):
     class BSWrapperForm(cls):
         def __init__(self, *args, **kw):
             super(cls, self).__init__(*args, **kw)
-            for name, field in self.fields.items():
-                self[name].css_classes = bsCssClasses
+            for field in self.fields.values():
                 field.widget.attrs['class'] = 'form-control'
 
         def as_bs(self):
             "Returns this form rendered as HTML <div>s."
             return self._html_output(
-            normal_row='<div class="form-group"%(html_class_attr)s>%(label)s %(field)s%(help_text)s</div>',
+            normal_row='<div%(html_class_attr)s>%(label)s %(errors)s%(field)s%(help_text)s</div>',
             error_row='%s',
             row_ender='</div>',
             help_text_html=' <span class="helptext">%s</span>',
             errors_on_separate_row=False
-        )
+            )
+
+        def __getitem__(self, name):
+            try:
+                field = self.fields[name]
+            except KeyError:
+                raise KeyError(
+                    "Key %r not found in %s" % (name, self.__class__.__name__))
+            return BSBoundField(self, field, name)
+
     return BSWrapperForm
+
