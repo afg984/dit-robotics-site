2ef3b44f44a94848f0e8aa30b6ed3714cf6c739a
afg984 <afg984@gmail.com>
2014-11-29 17:34:29 +0800

    fix: ViewDoesNotExist in accounts.urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login', dict(template_name='login.html'), name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.loguot', dict(template_name='home.html'), name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', dict(template_name='home.html'), name='logout'),
     #url(r'^/login/$', views.login_view, name='login'),
     #url(r'^/logout/$', views.logout_view, name='logout'),
 ]
