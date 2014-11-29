118d450c8fd29a5c38fe33f4a47f623432420827
afg984 <afg984@gmail.com>
2014-11-30 02:04:14 +0800

    redirect to login page after logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login', dict(template_name='login.html'), name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', dict(template_name='home.html'), name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', dict(next_page='login'), name='logout'),
     url(r'^registration/$', views.registration_view, name='registration'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^profile/([\w.@+-]+)/$', views.profile, name='profile'),
