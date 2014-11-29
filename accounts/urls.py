19c22617807fe126f5010c85c48490cc1b4a1694
afg984 <afg984@gmail.com>
2014-11-30 00:12:18 +0800

    add register in accounts.views

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login', dict(template_name='login.html'), name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', dict(template_name='home.html'), name='logout'),
+    url(r'^register/$', views.register_view, name='register'),
     #url(r'^/login/$', views.login_view, name='login'),
     #url(r'^/logout/$', views.logout_view, name='logout'),
 ]
