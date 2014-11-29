b2c8375c8de304d2831a4ae3f977fd7a733669eb
afg984 <afg984@gmail.com>
2014-11-29 18:48:39 +0800

    add filter to add class: form-control

---
+++ bootstrap_plugin/templatetags/filters.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+
+from django import template
+from django.utils.html import mark_safe
+
+register = template.Library()
+
+
+class IncorrectTagsSupported(Exception):
+    pass
+
+
+@register.filter
+def bs_form_control(value):
+    soup = BeautifulSoup(soup)
+    childrens = list(soup.html.body.children)
+    if len(childrens) != 1:
+        raise IncorrectTagsSupported(len(childrens))
+    tag = childrens[1]
+    try:
+        tag.attrs['class'].append('form-control')
+    except:
+        tag.attrs['class'] = 'form-control'
+    return mark_safe(str(tag))
