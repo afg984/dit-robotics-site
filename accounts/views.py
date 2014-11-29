869a70a936faeac8b316f827ba2b1c2f281bdcc4
afg984 <afg984@gmail.com>
2014-11-30 01:17:45 +0800

    create view for profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.forms import UserCreationForm
+from django.http import Http404
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -15,4 +17,15 @@ def registeration_view(request):
         form = UserCreationForm()
     context['form'] = form
     return render_to_response('registeration.html', context)
+
+def profile(request, username=None):
+    context = RequestContext(request)
+    if username is None:
+        if request.user.is_authenticated:
+            context['profileuser'] = request.user
+        else:
+            return redirect('login')
+    else:
+         context['profileuser'] = get_object_or_404(Users, username=username)
+    return render_to_response('profile.html', context)
 
