a08088878bb5f3f43b17af4c8036bf75382e417c
afg984 <afg984@gmail.com>
2014-11-30 01:39:29 +0800

    update accounts.urls for profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login', dict(template_name='login.html'), name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', dict(template_name='home.html'), name='logout'),
     url(r'^registeration/$', views.registeration_view, name='registeration'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile/([\w.@+-]+)/$', views.profile, name='profile'),
     #url(r'^/login/$', views.login_view, name='login'),
     #url(r'^/logout/$', views.logout_view, name='logout'),
 ]
