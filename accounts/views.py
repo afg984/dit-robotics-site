de435f5437a7192e379cc412924a4ba2e5af8942
afg984 <afg984@gmail.com>
2014-11-30 00:19:20 +0800

    crate registeration_view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.templates import RequestContext
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def registeration_view(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render_to_response('registeration.html')
+
