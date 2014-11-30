0970c5dbaef848b7f16380a7dc2b964b009b354a
afg984 <afg984@gmail.com>
2014-11-30 15:57:43 +0800

    create model for user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth import User
+from django.utils.html import format_html
 
 # Create your models here.
+class Profile(models.Model):
+    LEVEL_NAMES = ('NOEMAIL', 'USER', 'MEMBER', 'MOD', 'ADMIN')
+    LEVEL_CSS = ('muted', None, 'primary', 'warning', 'danger')
+    user = models.OneToOneField(User)
+    email = models.EmailField(blank=True, unique=True)
+    email_verified = models.BooleanField(default=False)
+
+    @property
+    def access_level(self):
+        if self.user.is_superuser:
+            return 4
+        elif False: # group moderator not implemented yet
+            return 3
+        elif self.email_verified:
+            if self.email in member_emails:
+                return 2
+            else:
+                return 1
+        else:
+            return 0
+
+    @property
+    def level_name(self):
+        return self.LEVEL_NAMES[self.access_level]
+
+    @property
+    def level_css(self):
+        return self.LEVEL_CSS[self.access_level]
+
+    @property
+    def html_link(self):
+        return format_html(
+            '<a class="{class_}" href="{href}">{username}</a>',
+            class_='' if self.level_css is None else "text-" + self.level_css,
+            href='', # should be link to profile
+            username = self.user.get_username(),
+        )
