3051450ccbde9e882bd3c6ac988a7a03a001ce15
afg984 <afg984@gmail.com>
2014-12-01 02:59:26 +0800

    deal with two message form variations in views

--- message_board/views.py
+++ message_board/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 
 from .models import Message
-from .forms import MessageForm
+from .forms import AnonymousMessageForm, UserMessageForm
 
 # Create your views here.
 def message_board(request):
     context = RequestContext(request)
+    if request.user.is_authenticated():
+        MessageForm = UserMessageForm
+    else:
+        MessageForm = AnonymousMessageForm
+
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
