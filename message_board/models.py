16b85f442df520453c1ffffe3bb08eac5cef2c18
afg984 <afg984@gmail.com>
2014-12-01 03:01:49 +0800

    allow Message.user to be null(Anonymous)

--- message_board/models.py
+++ message_board/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Message(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, null=True)
     nickname = models.CharField(max_length=31)
     message = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
