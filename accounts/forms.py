9dec1c21947be3d7f724d67fc085be404c251a31
afg984 <afg984@gmail.com>
2014-11-30 16:51:59 +0800

    rename ProfileForm to ProfileEmailForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from .models import Profile
 from bootstrap_plugin import simpleFactory
 
-class ProfileForm(simpleFactory(ModelForm)):
+class ProfileEmailForm(simpleFactory(ModelForm)):
     class Meta:
         model = Profile
         fields = ('email',)
