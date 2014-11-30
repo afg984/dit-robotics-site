d722fe5182d2ee2110fec3ccc864fab7e4fef1a9
afg984 <afg984@gmail.com>
2014-12-01 02:54:38 +0800

    add User as foreign key: Message.user

--- message_board/models.py
+++ message_board/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Message(models.Model):
+    user = models.ForeignKey(User)
     nickname = models.CharField(max_length=31)
     message = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
