f14b5ec033bab0242ba79f0b4443368ef774a756
afg984 <afg984@gmail.com>
2014-12-03 03:57:43 +0800

    /about/ view

--- drs/views.py
+++ drs/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
     return render_to_response('home.html', RequestContext(request))
+
+
+class AboutView(ListView):
+    template_name = 'about.html'
+    queryset = User.objects.filter(is_superuser=True)
+    context_object_name = 'maintainers'
