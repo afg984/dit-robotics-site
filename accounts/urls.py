ce5c6381a8822b0bb45668672f9e79511268ea7f
afg984 <afg984@gmail.com>
2014-12-03 02:01:49 +0800

    email verification tokens are 64 characters long

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^profile/([\w.@+-]+)/$', views.profile, name='profile'),
     url(r'^email/get-token/$', views.get_email_token, name='get_email_token'),
-    url(r'^email/verify/([0-9A-Za-z]{32})/$', views.verify_email, name='verify_email'),
+    url(r'^email/verify/([0-9A-Za-z]{64})/$', views.verify_email, name='verify_email'),
     url(r'^userlist/$',
         user_passes_test(lambda u: u.is_superuser)(views.UserList.as_view()),
         name='userlist'),
