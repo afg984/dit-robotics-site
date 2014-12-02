83f5bed7707ba710413c3858e70a6322a4e3ffc8
afg984 <afg984@gmail.com>
2014-12-03 03:58:54 +0800

    /about/ urls

--- drs/urls.py
+++ drs/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from . import settings
+from . import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'drs.views.home', name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.AboutView.as_view(), name='about'),
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^drive/', include('drive.urls')),
     url(r'^chinesc/', include('chinesc.urls')),
