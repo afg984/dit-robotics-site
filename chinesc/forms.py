68788fddd442e9bc23020489bddab52ee86abb6d
afg984 <afg984@gmail.com>
2014-12-03 04:03:11 +0800

    remove chinesc: remove project directory

--- chinesc/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from bootstrap_plugin.forms import simpleFactory
-
-class CodeForm(simpleFactory(forms.Form)):
-    user_code = forms.CharField(
-        label='Your Code',
-        widget=forms.Textarea()
-    )
