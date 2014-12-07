b5b839ab89d48070b2aeef5e4ad635c8a00d4779
afg984 <afg984@gmail.com>
2014-12-07 22:42:47 +0800

    Message objects __str__ show their content

--- message_board/models.py
+++ message_board/models.py
@@ -7,3 +7,6 @@ class Message(models.Model):
     nickname = models.CharField(max_length=31)
     message = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
