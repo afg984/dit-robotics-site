8cd7f17ed5841c3605140dc584b3b01ea1e0f5fd
afg984 <afg984@gmail.com>
2014-12-09 04:50:40 +0800

    test for accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class AccountTestCase(TestCase):
+    def setUp(self):
+        client = Client()
+        self.username = 'account_test_user'
+        self.password = 'account_test_password'
+        self.email = 'account_test_email@example.com'
+        self.registration_response = self.client.post(
+            reverse('registration'),
+            {
+                'username': self.username,
+                'password1': self.password,
+                'password2': self.password,
+                'email': self.email,
+            },
+        )
+        self.user = User.objects.get(username=self.username)
+
+    def test_redirect_registered_account_to_profile_page(self):
+        self.assertRedirects(self.registration_response, reverse('profile'))
+
+    def test_user_has_correct_email(self):
+        self.assertEqual(
+            self.user.email,
+            self.email
+        )
+
+    def test_user_has_profile(self):
+        self.user.profile
+    
+    def test_user_email_is_not_verified(self):
+        self.assertFalse(self.user.profile.email_verified)
+
+    def test_user_is_noemail(self):
+        self.assertEqual(self.user.profile.level_name, 'NOEMAIL')
