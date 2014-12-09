16a90c20572d928cff531c7bc5594107a7de75f0
afg984 <afg984@gmail.com>
2014-12-10 01:18:21 +0800

    edit view for uploading file

--- drive/views.py
+++ drive/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
+from .forms import UploadFileForm
 # Create your views here.
 
 def drive(request):
-    return render(request, 'drive.html')
+    context = RequestContext(request)
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            drive_file = form.save(commit=False)
+            drive_file.filename = 'NotImplemented'
+            drive_file.save()
+            return redirect('drive')
+    else:
+        form UploadFileForm()
+    context['form'] = form
+    return render_to_response('drive.html', context)
