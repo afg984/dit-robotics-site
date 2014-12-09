4ce9d39a1bc46b3585b1d0ad2d3cd26380dd3b01
afg984 <afg984@gmail.com>
2014-12-09 12:19:58 +0800

    move database to a folder

--- fix_perm.sh
+++ fix_perm.sh
@@ -3,5 +3,5 @@ chgrp afg .git -R
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
 chmod 755 manage.py
-chmod 775 .
-chmod 664 db.sqlite3
+chmod 775 database/
+chmod 664 database/db.sqlite3
