e20461ed22f9716677472bdffc2e8d297598ddf5
afg984 <afg984@gmail.com>
2014-12-10 14:53:35 +0800

    set session cookie age to 1 day

--- drs/settings.py
+++ drs/settings.py
@@ -93,6 +93,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+from django.utils.timezone import timedelta
+SESSION_COOKIE_AGE = timedelta(days=1) // timedelta(seconds=1)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
