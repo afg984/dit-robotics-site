c5fa7722350ef5cef0091647f35addf55e73f3e5
afg984 <afg984@gmail.com>
2014-12-12 01:07:58 +0800

    add helper property basename

--- drive/models.py
+++ drive/models.py
@@ -2,6 +2,7 @@ import os
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.html import format_html
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 def get_store_path(instance, filename):
@@ -12,9 +13,13 @@ class DriveFile(models.Model):
     user = models.ForeignKey(User)
     file = models.FileField(upload_to=get_store_path)
 
+    @property
+    def basename(self):
+        return os.path.basename(self.file.name)
+
     def as_link(self):
         return format_html(
             '<a href="{url}">{filename}</a>',
-            url=self.file.url,
-            filename=os.path.basename(self.file.name),
+            url=reverse('drive-get', args=[self.id]),
+            filename=self.basename,
         )
