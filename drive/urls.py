f8a36f4bbfecf90d197c1379b05cab5a6da78a5b
afg984 <afg984@gmail.com>
2014-12-12 01:58:11 +0800

    add delete view to urls

--- drive/urls.py
+++ drive/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.drive, name='drive'),
     url(r'^get/(\d+)/', views.get, name='drive-get'),
+    url(r'^delete/(\d+)/', views.delete, name='drive-delete'),
 ]
