310ad622f0b912698f7ce46d1e2af7b64b979560
afg984 <afg984@gmail.com>
2014-12-12 00:11:36 +0800

    add foreign key user in drivefile

--- drive/models.py
+++ drive/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class DriveFile(models.Model):
     filename = models.CharField(max_length=64)
+    user = models.ForeignKey(User)
     file = models.FileField()
