c273e323223ef3da5a374f3ac3322028f515db91
afg984 <afg984@gmail.com>
2014-12-12 00:40:26 +0800

    return files as rendered links

--- drive/models.py
+++ drive/models.py
@@ -1,6 +1,7 @@
 import os
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.html import format_html
 # Create your models here.
 
 def get_store_path(instance, filename):
@@ -10,3 +11,10 @@ class DriveFile(models.Model):
     filename = models.CharField(max_length=64)
     user = models.ForeignKey(User)
     file = models.FileField(upload_to=get_store_path)
+
+    def as_link(self):
+        return format_html(
+            '<a href="{url}">{filename}</a>',
+            url=self.file.url,
+            filename=os.path.basename(self.file.name),
+        )
