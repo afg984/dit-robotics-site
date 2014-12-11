94b81136d3b64a23d021bfbabd213fcc706a5f6e
afg984 <afg984@gmail.com>
2014-12-12 00:24:21 +0800

    add get_store_path

--- drive/models.py
+++ drive/models.py
@@ -1,8 +1,12 @@
+import os
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+def get_store_path(instance, filename):
+    return os.path.join('drive', instance.user, filename)
+
 class DriveFile(models.Model):
     filename = models.CharField(max_length=64)
     user = models.ForeignKey(User)
-    file = models.FileField()
+    file = models.FileField(upload_to=get_store_path)
