c1d0d5891d88ec55e1103f5f9fc37507daeccc9e
afg <afg984@gmail.com>
2014-12-12 17:10:33 +0800

    fix permissions

--- fix_perm.sh
+++ fix_perm.sh
@@ -3,5 +3,5 @@ chgrp afg .git -R
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
 chmod 755 manage.py
-chmod 775 database/
+chmod 775 database/ media/
 chmod 664 database/db.sqlite3
