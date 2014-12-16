f411e789ed03e9d83ea14dc52423eed87fa19040
afg984 <afg984@gmail.com>
2014-12-17 01:15:22 +0800

    show require member message to login user in drive

--- drive/views.py
+++ drive/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import RequestContext
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from drs import settings
@@ -9,14 +9,11 @@ from .forms import UploadFileForm
 from .models import DriveFile
 # Create your views here.
 
-def require_member(*args, **kw):
-    def is_member(user):
-        return user.is_authenticated() and user.profile.access_level > 1
-    return user_passes_test(is_member)(*args, **kw)
-
-@require_member
+@login_required
 def drive(request):
     context = RequestContext(request)
+    if request.user.profile.access_level < 2:
+        return render_to_response('drive_member_required.html', context)
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
