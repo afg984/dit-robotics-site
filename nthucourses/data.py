4260557d726ca964f0f68fc06c76a3952cc082fd
afg <afg984@gmail.com>
2015-01-05 20:20:37 +0800

    add times in data.py

--- nthucourses/data.py
+++ nthucourses/data.py
@@ -1,5 +1,12 @@
 import os
 import json
+import itertools
+
+
+weekdays = 'MTWRFS'
+classsects = '1234n56789abc'
+times = tuple(''.join(p) for p in itertools.product(weekdays, classsects))
+times_as_table = tuple(tuple(w + s for s in classsects) for w in weekdays)
 
 
 with open(os.path.join(os.path.dirname(__file__), 'courses.json')) as file:
