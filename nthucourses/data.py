60dc12ccafac6b3d2f2f2e45b7e07a5df2521a1a
afg <afg984@gmail.com>
2015-01-06 02:16:15 +0800

    new filter tool: CourseDataSet

--- nthucourses/data.py
+++ nthucourses/data.py
@@ -1,6 +1,7 @@
 import os
 import json
 import itertools
+import collections
 
 
 weekdays = 'MTWRFS'
@@ -9,5 +10,28 @@ times = tuple(''.join(p) for p in itertools.product(weekdays, classsects))
 times_as_table = tuple(tuple(w + s for s in classsects) for w in weekdays)
 
 
+class CourseDataSet(collections.UserDict):
+    def filter(self, function):
+        return type(self)(filter(lambda grp: function(grp[1]), self.items()))
+
+
+    def within_times(self, times):
+        return self.filter(
+            lambda ins: all(time in times for time in ins['time'])
+        )
+
+
+    def except_times(self, times):
+        return self.filter(
+            lambda ins: not any(time in times for time in ins['time'])
+        )
+
+
+    def within_departments(self, departments):
+        return self.filter(
+            lambda ins: ins['id'].startswith(departments)
+        )
+
+
 with open(os.path.join(os.path.dirname(__file__), 'courses.json')) as file:
-    data = json.load(file)
+    data = CourseDataSet(json.load(file))
