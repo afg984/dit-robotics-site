133b4883e4fef07b62bd9ecd7c1b3c00f28da222
afg <afg984@gmail.com>
2015-01-08 03:47:24 +0800

    nthucourses:indicate invalid search

--- nthucourses/views.py
+++ nthucourses/views.py
@@ -6,7 +6,10 @@ from .forms import CourseFilterForm
 
 def index(request):
     context = RequestContext(request)
-    form = CourseFilterForm(request.GET)
+    if request.GET:
+        form = CourseFilterForm(request.GET)
+    else:
+        form = CourseFilterForm()
     if form.is_valid():
         courses = data
         courses = courses.within_departments(form.cleaned_data['department'])
@@ -15,7 +18,5 @@ def index(request):
         else:
             courses = courses.within_times(form.cleaned_data['times'])
         context['courses'] = courses.values()
-    else:
-        form = CourseFilterForm()
     context['form'] = form
     return render_to_response('courses.html', context)
