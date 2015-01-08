b34c871ae060d1ea9decc4116971d6b52395974f
afg <afg984@gmail.com>
2015-01-08 20:06:49 +0800

    drive.urls: get/ becomes g/

--- drive/urls.py
+++ drive/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.drive, name='drive'),
     url(r'^l/((?:\w+/)+)$', views.listing, name='drive-listing'),
-    url(r'^get/(\d+)/(.+)$', views.get, name='drive-get'),
+    url(r'^g/(\d+)/(.+)$', views.get, name='drive-get'),
     url(r'^delete/(\d+)/$', views.delete, name='drive-delete'),
 ]
