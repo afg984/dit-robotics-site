a30ee2f3b93c8e97ea19692f1ba9e7f975b90580
afg <afg984@gmail.com>
2015-01-11 23:55:33 +0800

    nthucourses: [fix]sqlite cannot delete more than 999 queries at a time

--- nthucourses/management/commands/loadjsoncourses.py
+++ nthucourses/management/commands/loadjsoncourses.py
@@ -9,11 +9,34 @@ class Command(BaseCommand):
     args = '<jsonfile>'
     help = 'Update course data from json file'
 
+    def progress_iter(self, seq, msg):
+        total = len(seq)
+        width = len(str(total))
+        for n, item in enumerate(seq, start=1):
+            yield item
+            self.stdout.write(
+                '{msg}({n:{width}}/{total:{width}})'.format(
+                    msg=msg, n=n, width=width, total=total,
+                ),
+                ending='\r',
+            )
+        self.stdout.write('')
+
+    def delete_all(self, model):
+        while model.objects.count() >= 1000:
+            model.objects.latest('id').delete()
+            self.stdout.write('Deleteing...{:5}'.format(model.objects.count()), ending='\r')
+        model.objects.all().delete()
+        self.stdout.write('Deletion completed.')
+
     def handle(self, jsonfile, **options):
         with open(jsonfile) as file:
             jsondata = json.load(file)
-        Course.objects.all().delete()
-        for course in jsondata['courses'].values():
+        self.delete_all(Course)
+        for course in self.progress_iter(
+            jsondata['courses'].values(),
+            'Writing course...'
+        ):
             Course.objects.create(
                 time=''.join(course['time']),
                 number=course['no'],
@@ -26,4 +49,3 @@ class Command(BaseCommand):
                 title_zh=course['title_zh'],
                 note=course['note'],
             )
-        
