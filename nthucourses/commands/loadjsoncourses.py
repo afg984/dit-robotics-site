0ae064732c6eff22845de1ab6bfba6a8a84937a6
afg <afg984@gmail.com>
2015-01-11 23:13:50 +0800

    nthucourses: implement command for course data

---
+++ nthucourses/commands/loadjsoncourses.py
@@ -0,0 +1,29 @@
+import json
+
+from django.core.management.base import BaseCommand, CommandError
+
+from nthucourses.models import Course, Syllabus
+
+
+class Command(BaseCommand):
+    args = '<jsonfile>'
+    help = 'Update course data from json file'
+
+    def handle(self, jsonfile):
+        with open(jsonfile) as file:
+            jsondata = json.load(file)
+        Course.objects.all().delete()
+        for course in jsondata['courses'].values():
+            Course.objects.create(
+                time=''.join(course['time']),
+                number=course['no'],
+                capabilities=courses['capabilities'],
+                credit=courses['credit'],
+                enrollment=courses['enrollment'],
+                instructor=courses['instructor'],
+                room=courses['room'],
+                title_en=courses['title_en'],
+                title_zh=courses['title_zh'],
+                note=courses['note'],
+            )
+        
