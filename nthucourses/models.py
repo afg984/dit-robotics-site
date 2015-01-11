cc3be626657364df6b09f764226abaff5614e879
afg <afg984@gmail.com>
2015-01-11 22:53:28 +0800

    nthucourses: create model for course

--- nthucourses/models.py
+++ nthucourses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    time = CharField(max_length=20)
+    number = CharField(max_length=20)
+    capabilities = TextField()
+    credit = PositiveSmallIntegerField(null=True)
+    enrollment = PositiveSmallIntegerField()
+    instructor = CharField(max_length=20)
+    room = CharField(max_length=20)
+    title_en = CharField(max_length=40)
+    title_zh = CharField(max_length=40)
+    note = TextField()
