9c9f46f681760223f7ea5613b05213bda73e066f
afg <afg984@gmail.com>
2015-01-12 02:20:42 +0800

    nthucourses: update view for database-based data

--- nthucourses/views.py
+++ nthucourses/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from .data import courses as data
 from .forms import CourseFilterForm
+from .models import Department, Time
 
 def index(request):
     context = RequestContext(request)
@@ -11,14 +11,13 @@ def index(request):
     else:
         form = CourseFilterForm()
     if form.is_valid():
-        courses = data
-        courses = courses.within_departments(
-            [form.cleaned_data['department']]
-        )
+        courses = Department.objects.get(abbr=form.cleaned_data['department']).courses
+        time = Time.objects.filter(value__in=cleaned_data['times'])
         if form.cleaned_data['operation'] == 'except':
-            courses = courses.except_times(form.cleaned_data['times'])
+            for sect in time:
+                course = course.exclude(time__contains=sect)
         else:
-            courses = courses.within_times(form.cleaned_data['times'])
-        context['courses'] = courses.values()
+            courses = courses.filter(time__in=time)
+        context['courses'] = courses
     context['form'] = form
     return render_to_response('courses.html', context)
