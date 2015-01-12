e125f51f0dc3803869f5692aa3099d69df21b402
afg <afg984@gmail.com>
2015-01-12 13:52:05 +0800

    nthucourses: better time searching

--- nthucourses/views.py
+++ nthucourses/views.py
@@ -12,12 +12,11 @@ def index(request):
         form = CourseFilterForm()
     if form.is_valid():
         courses = Department.objects.get(abbr=form.cleaned_data['department']).courses
-        time = Time.objects.filter(value__in=form.cleaned_data['times'])
         if form.cleaned_data['operation'] == 'except':
-            for sect in time:
-                courses = courses.exclude(time__contains=sect)
+            time = Time.objects.exclude(value__in=form.cleaned_data['times'])
         else:
-            courses = courses.filter(time__in=time)
+            time = Time.objects.filter(value__in=form.cleaned_data['times'])
+        courses = courses.filter(time__in=time)
         context['courses'] = courses
     context['form'] = form
     return render_to_response('courses.html', context)
