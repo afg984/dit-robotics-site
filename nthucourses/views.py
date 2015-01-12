b8296e160e4440f2fbcc32b4123d881c8f588c30
afg <afg984@gmail.com>
2015-01-12 16:46:39 +0800

    nthucourses: do not use .all

--- nthucourses/views.py
+++ nthucourses/views.py
@@ -11,12 +11,12 @@ def index(request):
     else:
         form = CourseFilterForm()
     if form.is_valid():
-        courses = Department.objects.get(abbr=form.cleaned_data['department']).courses.all()
+        courses = Department.objects.get(abbr=form.cleaned_data['department']).courses
         if form.cleaned_data['operation'] == 'except':
             not_time = Time.objects.filter(value__in=form.cleaned_data['times'])
         else:
             not_time = Time.objects.exclude(value__in=form.cleaned_data['times'])
-        courses = courses.exclude(time__in=not_time)
+        courses = courses.exclude(time__in=not_time).distinct()
         context['courses'] = courses
     context['form'] = form
     return render_to_response('courses.html', context)
