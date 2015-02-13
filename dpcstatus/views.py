83f451703e79c6c99b1266003ea090387f8f07ba
afg <afg984@gmail.com>
2015-02-13 16:57:16 +0800

    dpcstatus: finish 3dp status

--- dpcstatus/views.py
+++ dpcstatus/views.py
@@ -1,10 +1,30 @@
+import json
+import traceback
+import urllib.request
+
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 # Create your views here.
 
+URL = 'http://192.168.100.12:5000'
+
+def get_data():
+    try:
+        return json.loads(urllib.request.urlopen(URL, timeout=3).read().decode())
+    except Exception:
+        return {'ERROR': traceback.format_exc()}
+    
+
 def index(request):
+    if not request.user.is_authenticated() or request.user.profile.access_level < 2:
+        return render_to_response(
+            'dpcstatus/denied.html',
+            context_instance=RequestContext(request)
+        )
     return render_to_response(
         'dpcstatus/index.html',
+        get_data(),
         context_instance=RequestContext(request)
     )
