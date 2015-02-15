36817a6f5315907c04676205ad9ceab924dd85e5
afg <afg984@gmail.com>
2015-02-16 00:18:29 +0800

    drive: actions do not end with "/"

--- drive/urls.py
+++ drive/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^l/(.+/)$', views.listing, name='drive-listing'),
     url(r'^lt/(.+/)$', views.listingtable, name='drive-listingtable'),
     url(r'^g/(\d+)/(.+)$', views.get, name='drive-get'),
-    url(r'^delete/(\d+)/$', views.delete, name='drive-delete'),
+    url(r'^delete/(\d+)$', views.delete, name='drive-delete'),
     url(r'^mkdir/(.+/)$', views.mkdir, name='drive-mkdir'),
     url(r'^rmdir/(\d+)$', views.rmdir, name='drive-rmdir'),
 ]
