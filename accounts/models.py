2ff98b35127c54bb66102147a257bd586cd4bfb2
afg <afg984@gmail.com>
2015-02-17 23:16:40 +0800

    accounts: Profile uses User as primary key

--- accounts/models.py
+++ accounts/models.py
@@ -36,7 +36,7 @@ class Profile(models.Model):
     LEVEL_NAMES = ('NOEMAIL', 'USER', 'MEMBER', 'MOD', 'ADMIN')
     LEVEL_CSS = ('muted', 'normal', 'success', 'warning', 'danger')
     TOKEN_LENGTH = 64
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, primary_key=True)
     email_verified = models.BooleanField(default=False)
     email_token = models.CharField(max_length=TOKEN_LENGTH, default="")
     email_token_expire = models.DateTimeField(null=True)
