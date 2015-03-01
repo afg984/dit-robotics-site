367014e2310d73cf6a2b5d91f0117334b197f938
afg <afg984@gmail.com>
2015-03-01 23:47:39 +0800

    dpcstatus: internal update

--- dpcstatus/urls.py
+++ dpcstatus/urls.py
@@ -6,5 +6,5 @@ from dpcstatus import views
 urlpatterns = [
     url(r'^$', views.index, kwargs={'template_name': 'dpcstatus/summary.html'}, name='index'),
     url(r'^details/$', views.index, kwargs={'template_name': 'dpcstatus/detail.html'}, name='details'),
-    url(r'^status-block/$', views.index, kwargs={'template_name': 'dpcstatus/pretty-status.html'}, name='status-block'),
+    url(r'^status-block/$', views.index, kwargs={'template_name': 'dpcstatus/pretty-status.html', 'summary': True}, name='status-block'),
 ]
--- dpcstatus/views.py
+++ dpcstatus/views.py
@@ -1,4 +1,3 @@
-import re
 import json
 import datetime
 import traceback
@@ -11,60 +10,50 @@ from django.shortcuts import render
 # Create your views here.
 
 URL = 'http://192.168.100.12'
+SUMMARY_URL = URL + '/summary/'
+
+def add_context(data):
+    printer = data['printer']
+    printer['timestamp'] = parse_datetime(printer['timestamp'])
+    printer['age'] = datetime.now() - printer['timestamp']
+    if 'starttime' in printer:
+        printer['starttime'] = datetime.fromtimestamp(printer['starttime'])
+    if 'secondsremain' in printer:
+        printer['remaining'] = datetime.timedelta(seconds=printer['remaining'])
+        printer['done'] = printer['timestamp'] + printer['remaining']
+    if 'status' in printer:
+        printer['statusstyle'] = {
+            'Printing': 'primary',
+            'SD Printing': 'primary',
+            'Disconnected': 'warning',
+            'Idle': 'success',
+        }.get(printer['status'], 'danger')
 
 
-pattern = re.compile(
-    r'Hotend:(?P<temp>[\d\.]+) E:\d+ W:\d+  Printing:(?P<perc>[\d\.]+) % \| Line# \d+of \d+ lines \| Est: (?P<est>\d\d\:\d\d\:\d\d) of: \d\d\:\d\d\:\d\d Remaining \|  Z: [\d\.]+ mm'
-)
-ptimepattern = re.compile(
-    r'(?P<hours>\d+)\:(?P<minutes>\d+)\:(?P<seconds>\d+)'
-)
-
-
-def add_pretty(datadict):
-    targetmsg = datadict['STATUS'][-1]
-    stuff = pattern.match(targetmsg['message'])
-    if stuff is None:
-        stuff = dict()
-        msg = targetmsg['message']
-        if '無法連接設備' in msg or 'is offline' in msg:
-            stuff['status'] = 'Disconnected'
-            stuff['statusstyle'] = 'warning'
-        elif msg.startswith('Loaded'):
-            stuff['status'] = 'Idle'
-            stuff['statusstyle'] = 'success'
-    else:
-        stuff = stuff.groupdict()
-        stuff['status'] = 'Printing'
-        stuff['statusstyle'] = 'primary'
-        if stuff['est'] is not None:
-            deltakw = ptimepattern.match(stuff['est']).groupdict()
-            for k in deltakw:
-                deltakw[k] = int(deltakw[k])
-            stuff['done'] = (parse_datetime(targetmsg['time']) + datetime.timedelta(**deltakw)).strftime('%m/%d %H:%M:%S')
-    datadict.update(stuff)
-
-
-def get_data():
+def get_data(url=URL):
     try:
-        data = json.loads(urllib.request.urlopen(URL, timeout=3).read().decode())
-        add_pretty(data)
+        data = json.loads(urllib.request.urlopen(url, timeout=1).read().decode())
+        add_context(data)
         return data
     except urllib.error.URLError:
         return {
             'ERROR': traceback.format_exc(),
-            'status': 'Computer Offline',
-            'statusstyle': 'danger',
+            'printer': {
+                'status': 'Computer Offline',
+                'statusstyle': 'danger',
+            }
         }
     except Exception:
         return {
             'ERROR': traceback.format_exc(),
-            'status': 'ERROR',
-            'statusstyle': 'danger',
+            'printer': {
+                'status': 'ERROR',
+                'statusstyle': 'danger',
+            }
         }
     
 
-def index(request, template_name):
+def index(request, template_name, summary=False):
     if not request.user.is_authenticated() or request.user.profile.access_level < 2:
         return render(
             request,
@@ -73,5 +62,5 @@ def index(request, template_name):
     return render(
         request,
         template_name,
-        get_data(),
+        get_data(SUMMARY_URL if summary else URL),
     )
