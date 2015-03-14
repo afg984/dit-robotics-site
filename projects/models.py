d6212038bd00b67629ff0400bb42b33161872333
afg <afg984@gmail.com>
2015-03-15 05:56:40 +0800

    Project.members can be empty

--- projects/models.py
+++ projects/models.py
@@ -10,7 +10,9 @@ class Project(models.Model):
     outline = models.CharField('簡介', max_length=96, blank=True)
     content = MarkdownTextField('內容')
     on_homepage = models.BooleanField('在首頁顯示', default=False)
-    members = models.ManyToManyField(User, verbose_name='成員')
+    members = models.ManyToManyField(User,
+        verbose_name='成員', blank=True
+    )
     priority = models.SmallIntegerField('優先度', default=0,
         help_text='排列優先度，由高至低。'
     )
