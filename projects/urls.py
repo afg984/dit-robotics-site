98e31484be37b0ed7d51abcf4c5c4396d4613080
afg <afg984@gmail.com>
2015-03-15 05:17:48 +0800

    integrate cover photo into home page

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,6 @@ from projects import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.ProjectIndex.as_view(), name='index'),
     url(r'^create/$', views.ProjectCreate.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', views.ProjectUpdate.as_view(), name='update'),
     url(r'^(?P<pk>\d+)/cover_photo/$', views.cover_photo, name='cover-photo'),
--- projects/views.py
+++ projects/views.py
@@ -7,12 +7,6 @@ from drive.http import AttachmentResponse
 from projects.models import Project
 
 
-class ProjectIndex(ListView):
-    model = Project
-    context_object_name = 'projects'
-    template_name = 'projects/index.html'
-
-
 class ProjectEditCommon:
     model = Project
     fields = '__all__'
