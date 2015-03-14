34247d800d10a29f24f771f2e5a2abe475bd1754
afg <afg984@gmail.com>
2015-03-15 01:37:57 +0800

    add model Project

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField('標題', max_length=48)
+    intro = models.CharField('簡介', max_length=48, blank=True)
+    content = models.TextField('內容')
+    on_homepage = models.BooleanField('在首頁顯示', default=False)
+    collaborators = models.ManyToManyField(User, verbose_name='成員')
+    cover_photo = models.ImageField('封面照片',
+        upload_to='projects/',
+        blank=True,
+    )
