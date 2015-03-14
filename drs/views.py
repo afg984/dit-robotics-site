56d74c4a617ae2eba27fccfe53a3669c34bc2111
afg <afg984@gmail.com>
2015-03-15 04:05:19 +0800

    update homepage for more context

--- drs/views.py
+++ drs/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView
 
 from projects.models import Project
 
 
-class AboutView(ListView):
+class AboutView(TemplateView):
     template_name = 'home.html'
-    queryset = Project.objects.filter(on_homepage=True)
-    context_object_name = 'projects'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'projects': Project.objects.all(),
+            'cover_projects': Project.objects.filter(on_homepage=True)
+        }
