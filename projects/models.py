19ff1eebe3540c5ecfa4e41906cd15aa3eda6fe2
afg <afg984@gmail.com>
2015-03-15 03:41:47 +0800

    display projects in profile page

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
     intro = models.CharField('簡介', max_length=48, blank=True)
     content = models.TextField('內容')
     on_homepage = models.BooleanField('在首頁顯示', default=False)
-    collaborators = models.ManyToManyField(User, verbose_name='成員')
+    members = models.ManyToManyField(User, verbose_name='成員')
     cover_photo = models.ImageField('封面照片',
         upload_to='projects/',
         blank=True,
