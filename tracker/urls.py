5bbe353bc2e0ea5bc97f85393ecf46765e9c78f5
afg <afg984@gmail.com>
2015-04-01 23:58:41 +0800

    can create workgroups

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,5 @@ from tracker import views
 
 urlpatterns = patterns('',
     url(r'^$', views.TrackerIndex.as_view(), name='index'),
+    url(r'^workgroups/$', views.WorkgroupCreate.as_view(), name='create_workgroup'),
 )
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
+from generic.views import LoginRequiredMixin
 from tracker.models import Workgroup
 
 
@@ -7,3 +9,9 @@ class TrackerIndex(ListView):
     model = Workgroup
     template_name = 'tracker/index.html'
     context_object_name = 'workgroups'
+
+
+class WorkgroupCreate(LoginRequiredMixin, CreateView):
+    model = Workgroup
+    template_name = 'tracker/workgroup_create.html'
+    fields = ('name',)
