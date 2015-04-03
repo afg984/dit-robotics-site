16270c58a28daeb4e259949493145d92b5d8bac6
afg <afg984@gmail.com>
2015-04-03 17:33:22 +0800

    customizable PermissionRequiredMixin

--- generic/views.py
+++ generic/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 
+
 class LoginRequiredMixin:
     @classmethod
     def as_view(cls, **initkwargs):
@@ -11,16 +12,22 @@ class LoginRequiredMixin:
 class SuperuserRequiredMixin(LoginRequiredMixin):
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_superuser:
-            return super(SuperuserRequiredMixin, self).dispatch(request, *args, **kwargs)
+            return super().dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
 
+
 class PermissionRequiredMixin(LoginRequiredMixin):
     def dispatch(self, request, *args, **kwargs):
+        if self.test_permission(self, request, *args, **kwargs):
+            return super().dispatch(request, *args, **kwargs)
+        raise PermissionDenied
+
+    def test_permission(self, request, *args, **kwargs):
         perms = self.permission_required
         if not isinstance(perms, (list, tuple)):
             perms = (perms,)
         if request.user.has_perms(perms):
-            return super(PermissionRequiredMixin, self).dispatch(request, *args, **kwargs)
+            return True
         else:
-            raise PermissionDenied
+            return False
