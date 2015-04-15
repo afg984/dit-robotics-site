aa62e390e29e9a4839a08ecc77ae7d790409716b
afg <afg984@gmail.com>
2015-04-15 22:47:35 +0800

    remove accounts.forms

--- accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-
-from bootstrap_plugin.forms import simpleFactory
-
-class EmailForm(simpleFactory(forms.Form)):
-    email = forms.EmailField()
