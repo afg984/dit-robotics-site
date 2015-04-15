017fe30d3fa78e2f0e536b249d40af2238476038
afg <afg984@gmail.com>
2015-04-15 22:47:00 +0800

    cleanup urls and views

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,13 +3,9 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', dict(next_page='login'), name='logout'),
-    url(r'^registration/$', views.registration_view, name='registration'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^profile/$', views.profile_redirect, name='profile'),
     url(r'^profile/([\w.@+-]+)/$', views.profile, name='profile'),
-    url(r'^email/get-token/$', views.get_email_token, name='get_email_token'),
-    url(r'^email/verify/([0-9A-Za-z]{64})/$', views.verify_email, name='verify_email'),
     url(r'^userlist/$',
         login_required(views.UserList.as_view()),
         name='userlist'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,57 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-from django.contrib.auth.views import login as django_login_view
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.core.mail import send_mail
-from django.core.urlresolvers import reverse
-from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic.list import ListView
 
 from .models import Profile
-from .forms import EmailForm
 
-# Create your views here.
-
-def login_view(request):
-    if request.user.is_authenticated():
-        return redirect(request.user.profile)
-    else:
-        return django_login_view(request, template_name='accounts/login.html')
-
-
-def registration_view(request):
-    if request.user.is_authenticated():
-        return redirect(request.user.profile)
-    context = {}
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        email_form = EmailForm(request.POST)
-        if form.is_valid() and email_form.is_valid():
-            user = form.save()
-            login(request,
-                authenticate(
-                    username=form.cleaned_data['username'],
-                    password=form.cleaned_data['password1'],
-                )
-            )
-            user.email = email_form.cleaned_data['email']
-            user.save()
-            Profile.objects.create(user=user)
-            messages.success(request, '註冊成功！請點選 VERIFY 以驗證你的 Email')
-            return redirect(request.user.profile)
-    else:
-        form = UserCreationForm()
-        email_form = EmailForm()
-    context['form'] = form
-    context['email_form'] = email_form
-    return render(request, 'accounts/registration.html', context)
 
 @login_required
 def profile_redirect(request):
     return redirect(Profile.objects.get_or_create(user=request.user)[0])
+
 
 def profile(request, username):
     context = {}
@@ -59,59 +17,6 @@ def profile(request, username):
     context['isself'] = (request.user == context['profileuser'])
     return render(request, 'accounts/profile.html', context)
 
-@login_required
-def get_email_token(request):
-    context = {}
-    if request.user.profile.email_verified:
-        context['error'] = 'Your email is already verified.'
-    else:
-        if request.method == 'GET':
-            if not request.user.profile.email_token_alive:
-                context['error'] = 'The email verification link does not exist.'
-            return render(request, 'accounts/email-sent.html', context)
-
-        body_template = '''\
-Dear {username},
-Please visit the link below to verify your email address:
-http://{host}{path}
-
-{host}'''
-
-        if request.user.email:
-            token = request.user.profile.get_email_token()
-            try:
-                success = send_mail(
-                    '{host} email verification'.format(host=request.get_host()),
-                    body_template.format(
-                        username=request.user.username,
-                        host=request.get_host(),
-                        path=reverse('verify_email', args=[token]),
-                    ),
-                    'noreply.ditrobotics@gmail.com',
-                    [request.user.email],
-                )
-            except Exception:
-                import traceback
-                context['error'] = traceback.format_exc().splitlines()[-1]
-            else:
-                if success:
-                    request.user.profile.refresh_email_token()
-                    return redirect('get_email_token')
-                else:
-                    context['error'] = 'The server failed to send an email to {}'.format(request.user.email)
-        else:
-            context['error'] = 'You have not set your email yet!'
-    return render(request, 'accounts/email-sent.html', context)
-
-def verify_email(request, token):
-    context = {}
-    vuser = get_object_or_404(Profile, email_token=token).user
-    context['vuser'] = vuser
-    if vuser.profile.email_token_expire > timezone.now():
-        vuser.profile.set_email_verified()
-        return render(request, 'accounts/email-verified.html', context)
-    else:
-        return render(request, 'accounts/email-link-expired.html', context)
 
 class UserList(ListView):
     context_object_name = 'all_users'
