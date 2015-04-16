70f8e25e3e88b0af87a7d3562de6f06fa28730f2
afg <afg984@gmail.com>
2015-04-16 20:32:00 +0800

    printer schedule alpha

--- printer_schedule/urls.py
+++ printer_schedule/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     url(r'^add/$', views.ScheduleCreate.as_view(), name='create'),
     url(r'^start/(?P<pk>\d+)/$', views.ScheduleStart.as_view(), name='start'),
     url(r'^end/(?P<pk>\d+)/$', views.ScheduleEnd.as_view(), name='end'),
+    url(r'edit/(?P<pk>\d+)/$', views.ScheduleEdit.as_view(), name='edit'),
+    url(r'gcode_file/(?P<pk>\d+)/(?P<fn>.+)$', views.ScheduleGCodeFile.as_view(), name='gcode-file'),
 )
--- printer_schedule/views.py
+++ printer_schedule/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.utils import timezone
+from django.core.exceptions import PermissionDenied
 
+from generic.views import LoginRequiredMixin
+from drive.http import AttachmentResponse
 from printer_schedule.models import PrinterSchedule
 
 
@@ -52,3 +57,21 @@ class ScheduleEnd(UpdateView):
         instance.ended_by = self.request.user
         instance.save()
         return redirect('printer_schedule:index')
+
+
+class ScheduleEdit(UpdateView):
+    model = PrinterSchedule
+    fields = ('gcode_file', 'est_duration')
+    template_name = 'printer_schedule/edit.html'
+    success_url = reverse_lazy('printer_schedule:index')
+
+
+class ScheduleGCodeFile(LoginRequiredMixin, DetailView):
+    model = PrinterSchedule
+
+    def get(self, request, pk, fn=None):
+        if request.user.profile.access_level > 1:
+            schedule = get_object_or_404(self.model, pk=pk)
+            return AttachmentResponse(file=schedule.gcode_file.file)
+        else:
+            raise PermissionDenied
